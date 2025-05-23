a7ea8ccaa62c41662695912a026a7c00660d2671
utkarsh-mishra19 <utk721mishra@gmail.com>
2025-05-23 10:12:36 +0530

    edited branch 1

--- kruskal.c
+++ kruskal.c
@@ -1,1 +1,24 @@
 #include<stdio.h>
+#include <stdlib.h>
+
+// ================================
+// Structures
+// ================================
+
+struct Edge {
+    int src, dest, weight;
+};
+
+struct Graph {
+    int V, E;
+    struct Edge* edge;
+};
+
+struct Subset {
+    int parent;
+    int rank;
+};
+
+// ================================
+// Function Declarations
+// ================================
